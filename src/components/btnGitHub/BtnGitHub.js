03cde45d828d87b1642b5a08950af8b24cb66467
Sparkoms <sparkoms@gmail.com>
2025-05-17 15:24:56 +0300

    Finished project

---
+++ src/components/btnGitHub/BtnGitHub.js
@@ -0,0 +1,13 @@
+import "./style.css";
+import git from "./gitHub-black.svg";
+
+const BtnGitHub = ({ link }) => {
+  return (
+    <a href={link} target="_blank" rel="noreferrer" className="btn-outline">
+      <img src={git} alt="git link" />
+      GitHub repo
+    </a>
+  );
+};
+
+export default BtnGitHub;
